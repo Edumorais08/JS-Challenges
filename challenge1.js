d985ae04d3292a786df0042b36bc8832dd21b83e
Eduardo Morais <edumorais08@gmail.com>
2023-11-27 11:40:43 -0300

    first challenge

---
+++ challenge1.js
@@ -0,0 +1,11 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+
+
+if (numeroUm == stringUm && numeroUm !== stringUm) {
+    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+  } else {
+    console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+  }
